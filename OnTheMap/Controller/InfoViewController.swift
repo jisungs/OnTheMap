b6daa23a17939ee927fa49db18659656113f90e3
jisung <jisung.smart@gmail.com>
2018-03-26 05:57:48 +0900

    Add info view controller
    There was a problem on TabBarController. I coudln't add navigation bar.
    I am finding a way.

---
+++ OnTheMap/Controller/InfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  InfoViewController.swift
+//  OnTheMap
+//
+//  Created by The book on 2018. 3. 26..
+//  Copyright © 2018년 The book. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class InfoViewController : UIViewController {
+    
+    var student: ParseStudent?
+    
+}
